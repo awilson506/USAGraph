7e2f6f80159806fc8c001445b97642def72691a3
Adam Wilson <awilson@pubsvs.com>
2014-12-08 17:27:02 -0500

    output file

--- src/Driver.java
+++ src/Driver.java
@@ -1,6 +1,13 @@
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+
 
 public class Driver {
 	public static void main(String args[]) {
+		try{
+		
+		PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
+		System.setOut(out);
 		
 		Graph g = new Graph();
 		g = BuildGraph.getMap(g);
@@ -8,6 +15,9 @@ public class Driver {
 		g.depthFirstTraversal(1);
 		System.out.println("\n");
 		g.breadthFirstTraversal(1);
+		}catch(Exception ex){
+			
+		}
 
 	}
 }
