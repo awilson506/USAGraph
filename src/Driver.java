db050cd787d04430f602480edc410e099b1c9969
Adam Wilson <awilson@pubsvs.com>
2014-12-08 17:32:08 -0500

    format

--- src/Driver.java
+++ src/Driver.java
@@ -1,21 +1,20 @@
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
-
 public class Driver {
 	public static void main(String args[]) {
-		try{
-		
-		PrintStream out = new PrintStream(new FileOutputStream("output.txt"));
-		System.setOut(out);
-		
-		Graph g = new Graph();
-		g = BuildGraph.getMap(g);
-		BuildGraph.assignStateName(g);
-		g.depthFirstTraversal(1);
-		System.out.println("\n");
-		g.breadthFirstTraversal(1);
-		}catch(Exception ex){
+		try {
+			PrintStream out = new PrintStream(
+					new FileOutputStream("output.txt"));
+			System.setOut(out);
+
+			Graph g = new Graph();
+			g = BuildGraph.getMap(g);
+			BuildGraph.assignStateName(g);
+			g.depthFirstTraversal(1);
+			System.out.println("\n");
+			g.breadthFirstTraversal(1);
+		} catch (Exception ex) {
 			System.out.println("Error: " + ex);
 		}
 
