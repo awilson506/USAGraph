65db8a3ed55284b3ff7fbc65b139fffe49e2fb52
Adam Wilson <awilson@pubsvs.com>
2014-12-10 11:21:08 -0500

    class comment

--- src/Driver.java
+++ src/Driver.java
@@ -1,3 +1,20 @@
+/*
+ * Name: Adam Wilson
+ * StudentID: 0543568
+ * Project: 6
+ * Class: COSC 600
+ * 
+ * This program uses the graph class to build a graph based on 
+ * the United States.  I reads in the vertices and edges from 
+ * input.txt based on the numbering of the inland states 1 through 48
+ * It then does a Breadth and Depth first traversal of the graph
+ * and prints out the level and state names that were visited.
+ * I worked on getting the four color theorem to work correctly
+ * but ran out of time trying to figure out a null pointer exception
+ * that I was getting.
+ * 
+ */
+
 import java.io.FileOutputStream;
 import java.io.PrintStream;
 
